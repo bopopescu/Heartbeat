6ff1327377cb65b3db9ee37cb1db7e7c87576c6f
Ben Augarten <baugarten@gmail.com>
2012-09-20 17:18:56 -0700

    Changed db settings

--- heartbeat/settings.py
+++ heartbeat/settings.py
@@ -12,14 +12,15 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'heartbeat',                      # Or path to database file if using sqlite3.
-        'USER': 'heartbeat',                      # Not used with sqlite3.
-        'PASSWORD': 'HliElyA19R29T',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    'default': dj_databases_url.config(default='postgres://localhost')
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': 'heartbeat',                      # Or path to database file if using sqlite3.
+#        'USER': 'heartbeat',                      # Not used with sqlite3.
+#        'PASSWORD': 'HliElyA19R29T',                  # Not used with sqlite3.
+#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#    }
 }
 
 # Local time zone for this installation. Choices can be found here:
