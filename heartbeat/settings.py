43d9ebc60166c495f10c518c856fb537b35cf1ee
Ben Augarten <b.augarten@gmail.com>
2012-09-27 23:46:39 -0700

    Updated settings.py to production version

--- heartbeat/settings.py
+++ heartbeat/settings.py
@@ -1,7 +1,8 @@
 # Django settings for heartbeat project.
 import os.path
+import dj_database_url
 
-DEBUG = True
+DEBUG = False 
 TEMPLATE_DEBUG = DEBUG
 PROJECT_DIR = os.path.dirname(__file__)
 
@@ -12,14 +13,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'heartbeat',                      # Or path to database file if using sqlite3.
-        'USER': 'heartbeat',                      # Not used with sqlite3.
-        'PASSWORD': 'HliElyA19R29T',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    'default': dj_database_url.config(default='postgres://localhost')
 }
 
 # Local time zone for this installation. Choices can be found here:
