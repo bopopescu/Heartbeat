362a0d4c5723c3a41df13d10e465d1cdec759a10
Ben Augarten <b.augarten@gmail.com>
2012-11-01 14:26:38 -0700

    Added gmail email settings

--- heartbeat/settings.py
+++ heartbeat/settings.py
@@ -17,6 +17,14 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': dj_database_url.config(default='postgres://localhost')
 }
+try:
+  EMAIL_USE_TLS = True
+  EMAIL_HOST = 'smtp.gmail.com'
+  EMAIL_HOST_USER = os.environ['GMAIL_SMTP_USER']
+  EMAIL_HOST_PASSWORD = os.environ['GMAIL_SMTP_PASSWORD']
+  EMAIL_PORT = 587
+except:
+  pass
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
