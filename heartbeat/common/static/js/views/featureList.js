609176d0e47df3aab4ccdeb156ba9452baf0a635
Ben Augarten <b.augarten@gmail.com>
2012-12-11 13:22:18 -0800

    dynamically add features to feature list

--- heartbeat/common/static/js/views/featureList.js
+++ heartbeat/common/static/js/views/featureList.js
@@ -6,53 +6,74 @@ define([
     'feature',
     'util',
     'text!templates/featureList.html',
+    'text!templates/feature.html',
     'text!templates/newFeature.html',
+    'views/featureView',
     'jquery.forms',
     'timeago',
-], function($, Backbone, _, Features, Feature, util, featureListTemplate, newFeatureTemplate) {
+], function($, Backbone, _, Features, Feature, util, featureListTemplate, featureTemplate, newFeatureTemplate, FeatureView) {
   FeatureListView = Backbone.View.extend({
     el: $("#features"),
     events: {
       "click #addfeature": "openform",
-      "click .icon-thumbs-up": "upvote",
-      "click .icon-thumbs-down": "downvote",
+    //  "click .icon-thumbs-up": "upvote",
+    //  "click .icon-thumbs-down": "downvote",
     },
     defaults: {
       user_id: -1,
+      list: ".featurelist",
+      index: 0,
     },
     initialize: function(options) {
-      _.bindAll(this, 'render', 'openform', 'vote');
-      this.collection.bind("change", this.render);
+      _.bindAll(this, 'render', 'openform', 'add');
+      this.collection.bind("add", this.add);
       this.options = _.extend(this.defaults, options);
     },
+    add: function(model, silent) {
+      model.bind("change", this.rerender);
+      var toAppend = new FeatureView({
+        model: model,
+        index: this.options.index,
+        user_id: this.options.user_id,
+      }).render().$el;
+      if (silent === true) {
+        toAppend.appendTo($(this.options.list));
+      } else {
+        toAppend.hide().appendTo($(this.options.list)).slideDown('slow');
+        $(".timeago").timeago();
+      }
+      this.options.index += 1;
+    },
     render: function() {
-      console.log(this.collection)
+      var self = this;
       $(this.el).html(_.template(featureListTemplate, { 
         features: this.collection, 
         user_id: this.options.user_id 
       }));
+      this.collection.each(function(model) { self.add(model, true); });
       $(".timeago").timeago();
     },
     openform: function() {
+      var self = this;
       if (!this.options.open) {
         this.options.open = true;
-        $(_.template(newFeatureTemplate, { user_id: this.options.user_id })).hide().appendTo(this.el).slideDown('medium');
-        var that = this;
+        $(_.template(newFeatureTemplate, { 
+          user_id: this.options.user_id 
+        })).hide().appendTo(this.el).slideDown('medium');
+
         $(this.el).find("#nevermind").click(function() {
-          that.options.open = false;
-          $(that.el).find("form").remove();
+          self.options.open = false;
+          $(self.el).find("form").remove();
         });
         $(this.el).find("form").submit(function() {
           new Feature().save({
             slug: $("#slug").val(),
-            created_by: util.userResource(that.options.user_id),
+            created_by: util.userResource(self.options.user_id),
             accepted: $("#accepted").val(),
             rejected: $("#rejected").val(),
           }, {
-            success: function(a,b,c) {
-              console.log(a);         
-              console.log(b);         
-              console.log(c);         
+            success: function(model) {
+              self.collection.add(model);
             },
             error: function(a,b,c) {
               console.log(a);         
@@ -64,6 +85,7 @@ define([
         });
       }
     },
+    /*
     upvote: function(event) {
       var target = $(event.currentTarget);
       this.vote(parseInt(target.data("index")), target.attr('id'), true);
@@ -104,6 +126,7 @@ define([
         },
       });
     },
+    */
   });
   Vote = Backbone.Model.extend({
     url: function() {
