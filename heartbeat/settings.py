97f138f030f85a51c7bbd8d816c7e4a63a15c460
Ben Augarten <b.augarten@gmail.com>
2012-12-14 13:46:40 -0800

    Added settings for aws

--- heartbeat/settings.py
+++ heartbeat/settings.py
@@ -68,6 +68,14 @@ MEDIA_ROOT = os.path.join(PROJECT_DIR, "media")
 #MEDIA_URL = '/media/'
 MEDIA_URL = '/media/'
 
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = S3_URL
+
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
@@ -76,7 +84,7 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+#
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -180,6 +188,7 @@ INSTALLED_APPS = (
     'tagging',
     'emencia.django.newsletter',
     'gunicorn',
+    'storages',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
